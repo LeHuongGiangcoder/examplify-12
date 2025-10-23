1817bc861454271f36d236e8798a4cec27b19561
LeHuongGiangcoder <hgiangle.work@gmail.com>
2025-10-24 00:35:40 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import MockExams from "./pages/MockExams";
 import TopicPractice from "./pages/TopicPractice";
+import { ExamResult } from "./pages/ExamResult";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/mock-exams" element={<MockExams />} />
           <Route path="/topic-practice" element={<TopicPractice />} />
+          <Route path="/exam/:examId/result" element={<ExamResult />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
